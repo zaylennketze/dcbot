ac1efab7f3374683ef5af2ab1ef2edde0570789c
zaylennketze <zaylenn.ketze@hotmail.com>
2026-08-07 23:12:08 +0000

    fghjk

--- src/utils/presence.js
+++ src/utils/presence.js
@@ -3,11 +3,25 @@ const { ActivityType } = require('discord.js');
 const updatePresence = async (client) => {
   if (!client.user) return;
 
+  const defaultText = `Your Server | ${client.guilds.cache.size} servers`;
+  const activities = [
+    {
+      name: defaultText,
+      type: ActivityType.Watching
+    }
+  ];
+
   const globalBio = client.storage?.getSetting('global', 'botActivityText');
-  const defaultText = `Your Server ^^ | ${client.guilds.cache.size} servers`;
-  const activityText = globalBio || defaultText;
-  return client.user.setActivity(activityText, {
-    type: ActivityType.Watching
+  if (globalBio) {
+    activities.unshift({
+      name: globalBio,
+      type: ActivityType.Custom
+    });
+  }
+
+  return client.user.setPresence({
+    activities,
+    status: 'online'
   });
 };
 
